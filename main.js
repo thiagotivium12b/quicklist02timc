fc3aa5b17a2cdf6e09b17054e96eb0af1fca5b5b
Thiago Tivium <thiagotivium777@gmail.com>
2025-06-06 23:28:31 -0300

    criação da função showitemlist para redenrizar os itens do array na tela

--- main.js
+++ main.js
@@ -10,10 +10,37 @@ function additem() {
     checked : false
  }
 
- items.push(item)
+    items.push(item)
 
- document.querySelector("#item").value = ""
+    document.querySelector("#item").value = ""
 
- console.log(items)
- 
+    showitemsList()
+
 }
+
+function showitemsList() {
+    const sectionList = document.querySelector(".list")
+    sectionList.textContent = "nada" 
+
+
+    items.map ((item, index ) => ){
+        sectionList.innerHTML +=
+        <div class="item">
+            <div>
+                <input type="checkbox" name="list" id="item-1">
+                <div class="custom-checkbox">
+                    <img src="./assets/checked.svg" alt="checked">
+                    <label for="item-${index}>${item.name}</label>
+                </div>
+
+                <button>
+                    <img src="./assets/trash-icon.svg" alt="trash-icon">
+                </button>
+            </div>
+           
+        
+
+    }
+
+
+
