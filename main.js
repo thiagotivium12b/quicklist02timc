9fbf39e6233a04bbcbe24c59f9d4603f60b05b20
Thiago Tivium <thiagotivium777@gmail.com>
2025-06-08 23:22:44 -0300

    criação da função para remover os itens do array: removeitems

--- main.js
+++ main.js
@@ -33,14 +33,32 @@ function showitemsList() {
                     <label for="item-${index}>${item.name}</label>
                 </div>
 
-                <button>
+                <button-onclick="removeitem("">
                     <img src="./assets/trash-icon.svg" alt="trash-icon">
-                </button>
+                </button-onclick=>
             </div>
            
+
+           function removeitem(itemname) {
+            const itemindex = items.findindex((item) => item.name === itemName)
+            const divWarning = document.querySelector(".warning")
+             
+            divWarning.classList.remove("-hide-warning")
+
+            if(itemIndex !== -1) {
+                items.splice(itemIndex, 1)
+            }
+            
+
+            snowitemsList() 
+
+
+ }
+
+
         
 
-    }
+    
 
 
 
