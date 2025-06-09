a8b2e6b810d0ac73973497046ecde551249ae3d5
Thiago Tivium <thiagotivium777@gmail.com>
2025-06-08 23:46:07 -0300

    criação da função checkItem para marcar e desmarcar itens do array

--- main.js
+++ main.js
@@ -27,10 +27,10 @@ function showitemsList() {
         sectionList.innerHTML +=
         <div class="item">
             <div>
-                <input type="checkbox" name="list" id="item-1">
-                <div class="custom-checkbox">
+                <input type="checkbox" name="list" id="item-1" ${item.checked && "checked"}>
+            <div class="custom-checkbox" onclick="checkItem('${item.name}')">
                     <img src="./assets/checked.svg" alt="checked">
-                    <label for="item-${index}>${item.name}</label>
+                    <label for="item-${index}" onclick=">${item.name}">${item.name}</label>
                 </div>
 
                 <button-onclick="removeitem("">
@@ -38,6 +38,16 @@ function showitemsList() {
                 </button-onclick=>
             </div>
            
+function checkItem(itemName) {
+    const item = items.find((item) => item.name === itemName)
+    if (item.checked === false){
+    item.checked = true
+    } else {
+        item.checked = false
+    } 
+
+}
+
 
            function removeitem(itemname) {
             const itemindex = items.findindex((item) => item.name === itemName)
